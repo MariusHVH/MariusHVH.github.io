512cb1e0f356f6a38f465dac2434977d67558584
skid1336 <137450720+MariusHVH@users.noreply.github.com>
2025-10-14 16:29:03 +0300

    Delete assets directory

--- assets/scripts/redirect.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  const currentURL = window.location.href;
-  const newURL = currentURL.replace(/\.html$/, "");
-
-  if (newURL !== currentURL) {
-    window.location.replace(newURL);
-  }
-};
