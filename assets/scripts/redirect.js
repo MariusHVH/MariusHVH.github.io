cccf5e00bde5658ff0d3a782acef4a0888032116
skid1336 <137450720+MariusHVH@users.noreply.github.com>
2025-10-14 16:08:01 +0300

    Add files via upload

---
+++ assets/scripts/redirect.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  const currentURL = window.location.href;
+  const newURL = currentURL.replace(/\.html$/, "");
+
+  if (newURL !== currentURL) {
+    window.location.replace(newURL);
+  }
+};
